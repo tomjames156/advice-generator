f3eb9852d8fe7dd7625f80b7ab3549f927a934a5
Akinwande Tomisin <tomjames156@gmail.com>
2022-10-15 11:27:34 +0100

    added images and removed react app template files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-dark-bg w-screen h-screen grid grid-cols-1 justify-center items-center">
+      <div className="w-[25rem] h-[15rem] mx-auto bg-gray-bg p-4 flex flex-col gap-4 text-center rounded-xl">
+        <h2 className="text-green-bg">ADVICE #117</h2>
+        <p className="text-whitish-text font-bold text-xl indent-[2.5rem]">"It is easy to sit up and take notice what is difficult is getting up and taking action"</p>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      "dark-bg": "#202632",
+      'gray-bg': '#343a49',
+      'green-bg': '#53ffa9',
+      'whitish-text': "#cde4e9"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
